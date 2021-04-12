150bb8a1da46d7d7a1bc3c8c2d34c9dda8230fe3
In8finity <wintry3@gmail.com>
2021-04-12 22:02:56 +0300

    Add abstract Remove/Add StateSpell
    Used to eliminate copypaste in alike spells
    for manipulating states

---
+++ Spells/AddStateSpell.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace game
+{
+    class AddStateSpell : Spell
+    {
+        protected AddStateSpell(double minManaCost, bool hasMotionalComponent, bool hasVerbalComponent, State stateToAdd) : base(minManaCost, hasMotionalComponent, hasVerbalComponent)
+        {
+            StateToAdd = stateToAdd;
+        }
+
+        public State StateToAdd { get; }
+
+        public override void MagicEffect(Magician user, Character target)
+        {
+            if (base.CheckCastRequirements(user))
+            {
+                if (target.AddState(StateToAdd))
+                {
+                    user.Mana -= MinManaCost;
+                }
+            }
+        }
+
+        public  void MagicEffect(Magician user)
+        {
+            MagicEffect(user, user);
+        }
+
+    }
+}
---
+++ Spells/RemoveStateSpell.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace game
+{
+    internal abstract class RemoveStateSpell : Spell
+    {
+        protected RemoveStateSpell(double minManaCost, bool hasMotionalComponent, bool hasVerbalComponent, State stateToRemove) : base(minManaCost, hasMotionalComponent, hasVerbalComponent)
+        {
+            StateToRemove = stateToRemove;
+        }
+
+        public State StateToRemove { get; }
+
+        public override void MagicEffect(Magician user, Character target)
+        {
+            if (base.CheckCastRequirements(user))
+            {
+                if (target.RemoveState(StateToRemove))
+                {
+                    user.Mana -= MinManaCost;
+                }
+            }
+        }
+
+    }
+}
