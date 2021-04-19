715b3b723d897c499dc07775c54a4ada3dc443f1
Veta <lizavetka1428@gmail.com>
2021-04-19 16:25:43 +0300

    FightPlan class added.

---
+++ Adapters/FightPlan.cs
@@ -0,0 +1,22 @@
+﻿using game;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KashTaskWPF.Adapters
+{
+    class FightPlan
+    {
+        public FightPlan()
+        {
+
+        }
+
+        public List<Magician> enemyM { get; set; }
+        public List<Character> enemyNonM { get; set; }
+        public List<Dictionary<string, List<string>>> dialog;
+
+    }
+}
